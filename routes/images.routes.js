51fd0a867e3bcb25938c2884db7444ee256d5616
Entangled-Logic Alpha <entangledlogic@outlook.com>
2025-09-01 02:10:22 -0700

    Update images.routes.js

--- routes/images.routes.js
+++ routes/images.routes.js
@@ -1,33 +1,25 @@
 const express = require('express');
 const multer = require('multer');
-
 const path = require('path');
 const imageController = require('../controllers/images.controller');
-
-const router = express.Router();
 const { v4: uuidv4 } = require('uuid');
 
-const storage = multer.diskStorage({
+const router = express.Router();
 
+const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'images/');
+    cb(null, '/srv/www/NeonServer/images');
   },
   filename: (req, file, cb) => {
-
     const ext = path.extname(file.originalname);
-    
     const uniqueName = `${uuidv4()}${ext}`;
-    
     cb(null, uniqueName);
-
   }
-
 });
 
 const upload = multer({ storage, limits: { fileSize: 4194304 } });
 
 router.post('/', upload.single('file'), imageController.uploadImage);
-
 router.delete('/:filename', imageController.deleteImage);
-  
+
 module.exports = router;
